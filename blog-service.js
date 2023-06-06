cf017db9975b7c2513e4a8e448d1963408cb04fb
Mohsen Sabet <msabetkoohenjani@myseneca.ca>
2023-06-05 23:11:23 -0400

    final Commit

---
+++ blog-service.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+
+let posts = [];
+let categories = [];
+
+function readJsonFile(file) {
+    return fs.readFile(`./data/${file}`, 'utf8').then(data => JSON.parse(data));
+}
+
+exports.initialize = function() {
+    return Promise.all([readJsonFile('posts.json'), readJsonFile('categories.json')]).then(data => {
+        [posts, categories] = data;
+    }).catch(err => {
+        console.error(`Error reading data files: ${err}`);
+        throw err;
+    });
+}
+
+exports.getAllPosts = function() {
+    return posts.length ? Promise.resolve(posts) : Promise.reject('No posts found');
+}
+
+exports.getPublishedPosts = function() {
+    const publishedPosts = posts.filter(post => post.published);
+    return publishedPosts.length ? Promise.resolve(publishedPosts) : Promise.reject('No published posts found');
+}
+
+exports.getCategories = function() {
+    return categories.length ? Promise.resolve(categories) : Promise.reject('No categories found');
+}
---
+++ server.js
@@ -0,0 +1,48 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Mohsen Sabet Student ID: 113205165 Date: 2023-06-05
+*  Cyclic Web App URL: ________________________________________________________
+*
+*  GitHub Repository URL: ______________________________________________________
+*
+********************************************************************************/ 
+
+const express = require('express');
+const path = require('path');
+const blogService = require('./blog-service');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect('/about');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/about.html'));
+});
+
+app.get('/blog', (req, res) => {
+    blogService.getPublishedPosts().then(data => res.json(data)).catch(err => res.json({message: err}));
+});
+
+app.get('/posts', (req, res) => {
+    blogService.getAllPosts().then(data => res.json(data)).catch(err => res.json({message: err}));
+});
+
+app.get('/categories', (req, res) => {
+    blogService.getCategories().then(data => res.json(data)).catch(err => res.json({message: err}));
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page Not Found');
+});
+
+blogService.initialize().then(() => {
+    app.listen(port, () => console.log(`Express http server listening on port ${port}`));
+}).catch(err => console.error(`Error initializing blog service: ${err}`));
